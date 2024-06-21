12b5faf15991ddc97d7cfb0b04d7162029776346
divyeshS81 <divyeshsarvaiya999@gmail.com>
2024-06-21 18:57:13 +0530

    fetch video data and show to screen

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{ useState,  useEffect } from 'react'
 import './PlayVideo.css'
 import video1 from "../../assets/video.mp4"
 import like from "../../assets/like.png"
@@ -7,15 +7,30 @@ import share from "../../assets/share.png"
 import save from "../../assets/save.png"
 import jack from "../../assets/jack.png"
 import user_profile from "../../assets/user_profile.jpg"
+import {API_KEY, value_converter} from "../../data"
+import moment from 'moment'
 
 const PlayVideo = ({videoId}) => {
+
+    const [apiData,setApiData] = useState(null);
+
+    const fetchVideoData = async () => {
+        // Fetching videos data
+        const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY} `;
+        await fetch(videoDetails_url).then(res=>res.json()).then(data => setApiData(data.items[0]));
+    }
+    useEffect(() => {
+        fetchVideoData();
+    }, [])
+
+
   return (
     <div className='play-video'>
         {/* <video src={video1} controls autoPlay muted ></video> */}
         <iframe width="853" height="480" src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <h3>Best Youtube Channel To Learn Web Development</h3>
+        <h3>{apiData?apiData.snippet.title:"Title Here"}</h3>
         <div className="play-video-info">
-            <p>1525 Views &bull; 2 days ago</p>
+            <p>{apiData?value_converter(apiData.statistics.viewCount):"16K"} Views &bull; {apiData?moment(apiData.snippet.publishedAt).fromNow():""}</p>
             <div>
                 <span> <img src={like} alt="" /> 125 </span>
                 <span> <img src={dislike} alt="" /> 2 </span>
